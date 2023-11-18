07e0bc84af817157480f5578d815bd583ef49c8f
ZiJson <jasonnosajya53@gmail.com>
2023-11-18 23:33:39 +0800

    debug user pannel

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -39,7 +39,7 @@ export default async function Page({ params }: { params: { slug: string } }) {
           )}
         </div>
       </div>
-      <UserPannel inAdmin={false}/>
+      {/* <UserPannel inAdmin={false}/> */}
     </>
   )
 }
